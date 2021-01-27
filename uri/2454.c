de879137ca0c39266e4cb191a22df9e591d87aaa
Erick levy <48847770+Ericklevy@users.noreply.github.com>
2021-01-27 02:22:21 -0300

    Create 2454.c

---
+++ uri/2454.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int P,R;
+    scanf("%d",&P);
+    scanf("%d",&R);
+        if(P==1){
+        if(R==1){
+            printf("A");
+        }
+        else if(R==0){
+            printf("B");
+        }
+    }
+        if(P==0){
+        printf("C");
+        }
+return 0;
+}
