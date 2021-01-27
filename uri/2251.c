d27b64020ba154a42b38ae259ef1e43b4695c8af
Erick levy <48847770+Ericklevy@users.noreply.github.com>
2021-01-27 01:36:51 -0300

    Create 2251.c

---
+++ uri/2251.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  //torre de hanoi
+  int n, teste=1;
+  while (scanf("%d", &n) == 1 && n > 0)
+    {
+      printf("Teste %d\n%d\n\n", teste++, (1 << n) - 1);
+    }
+  return 0;
+}
